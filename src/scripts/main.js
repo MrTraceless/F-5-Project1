30334d96f9971f71db214138505ee4f0975043eb
Yaroslav Danilik <yarosalvdaniliK@gmail.com>
2019-03-03 21:55:10 +0200

    LF replaced by CRLF

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,16 @@ $(document).ready(function() {
         grid.masonry('layout');
     });
 });
+
+
+//Google map API
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 49.8204423, lng: 24.0574003};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 17, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
