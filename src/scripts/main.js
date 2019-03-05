bf1543dbc3e3f549c72daafd0ece93c42b1c310a
Serhii <serhey.f5@gmail.com>
2019-03-05 13:25:58 +0200

    fill burger-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,10 +12,46 @@ $(document).ready(function() {
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: 49.8204423, lng: 24.0574003};
+  var uluru = {lat: 39.5323569, lng: -119.8091726};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 17, center: uluru});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
+
+
+//Burger Menu
+
+var Menu = {
+  
+    el: {
+      ham: $('.menu'),
+      menuTop: $('.menu-top'),
+      menuMiddle: $('.menu-middle'),
+      menuBottom: $('.menu-bottom')
+    },
+    
+    init: function() {
+      Menu.bindUIactions();
+    },
+    
+    bindUIactions: function() {
+      Menu.el.ham
+          .on(
+            'click',
+          function(event) {
+          Menu.activateMenu(event);
+          event.preventDefault();
+        }
+      );
+    },
+    
+    activateMenu: function() {
+      Menu.el.menuTop.toggleClass('menu-top-click');
+      Menu.el.menuMiddle.toggleClass('menu-middle-click');
+      Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+    }
+  };
+  
+  Menu.init();
